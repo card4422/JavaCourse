4c646fd66ef009eb99ef217692a06004e5479158
zhuzheng <ecnu_zz@163.com>
2016-12-23 21:05:29 +0800

    Add question_3

---
+++ src/question_4/A.java
@@ -0,0 +1,42 @@
+package question_4;
+
+import java.util.Date;
+
+/**
+ * Created by zhuzheng on 16/12/23.
+ */
+public class A {
+    int a;
+    String b;
+    Date c;
+
+    public Date getC() {
+        return c;
+    }
+
+    public void setC(Date c) {
+        this.c = c;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void init(){
+        a = 110;
+        b = "jizhi";
+        c = new Date();
+    }
+}
